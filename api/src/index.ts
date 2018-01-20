c700fd09c4831e40e0f3f29ce84c7bbec44da0a0
Romain Carrette <romain.carrette@live.fr>
2018-01-20 22:21:29 +0100

    fixed TS string interpolation

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000
 
 server.listen(port, (err) => {
     return console.log(err ?
-        'server failed to start: ${err}' :
-        'server running on port ${port}')
+        `server failed to start: ${err}` :
+        `server running on port ${port}`)
 })
