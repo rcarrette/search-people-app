8b3d328649367a556b68e5c9e69ce3c84acb414a
rcarrette <romain.carrette@live.fr>
2018-02-06 16:17:02 +0100

    deleted MongoDB api routes, as they are now handled by GraphQL endpoint

--- api/src/server.ts
+++ api/src/server.ts
@@ -19,23 +19,6 @@ class Server {
 
         this.express.use('/api/people', router)  //base prefix
 
-        router.get('/', async (req: express.Request, res: express.Response) => {
-            let people = await mongoose.getPeopleAsync(),
-                personsCount: Number = people.length
-
-            res.json(personsCount > 0 ?
-                people :
-                'no persons found'
-            )
-        })
-
-        //router.get for easy testing. TODO add person parameter + must be changed to post + error handling
-        router.get('/add', async (req: express.Request, res: express.Response) => {
-            let result = await mongoose.addPeopleAsync()
-
-            res.json(result)
-        })
-
         //router.get for easy testing. TODO add person parameter + must be changed to post + error handling
         router.get('/update', async (req: express.Request, res: express.Response) => {
             let result = await mongoose.updatePeopleAsync(),
