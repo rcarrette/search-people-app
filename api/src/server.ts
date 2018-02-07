c4b7f3fb192dd98eb790d0452b298d185c3a53d9
rcarrette <romain.carrette@live.fr>
2018-02-07 11:37:13 +0100

    deleted last Mongo route

--- api/src/server.ts
+++ api/src/server.ts
@@ -15,22 +15,6 @@ class Server {
     private buildRoutes(): void {
         const router = express.Router()
 
-        //#region MongoDB routes
-
-        this.express.use('/api/people', router)  //base prefix
-
-        //router.get for easy testing. TODO add person parameter + must be changed to post + error handling
-        router.get('/update', async (req: express.Request, res: express.Response) => {
-            let result = await mongoose.updatePeopleAsync(),
-                message = result &&
-                    `person with id '${result._id}' updated` ||
-                    'no persons updated'
-
-            res.json(message)
-        })
-
-        //#endregion
-
         //#region GraphQL routes
 
         this.express.use('/graphql', graphqlHTTP({
