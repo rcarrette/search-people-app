da5981f7fa943b5182c50e47c0063e9530c24c5b
rcarrette <romain.carrette@live.fr>
2018-02-07 13:49:38 +0100

    removed Mongo testing and prepared environment for GraphQL testing

---
+++ api/tests/graphql/schema/people.test.ts
@@ -0,0 +1,12 @@
+import * as graphql from 'graphql'
+import * as chai from 'chai'
+import PeopleSchema from '../../../src/graphql/schemas/people'
+
+const expect = chai.expect
+
+describe('People', () => {
+    it('Should have a firstName field of type String', () => {
+        //TODO expect(...)
+        console.log(PeopleSchema)
+    })
+})
