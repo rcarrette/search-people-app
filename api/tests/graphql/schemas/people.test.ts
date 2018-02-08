eca2cbae55b61e81eb4ec91f085b0b023379e49f
rcarrette <romain.carrette@live.fr>
2018-02-08 13:30:00 +0100

    added first GraphQL resolvers test

--- api/tests/graphql/schemas/people.test.ts
+++ api/tests/graphql/schemas/people.test.ts
@@ -1,6 +1,7 @@
 import * as graphql from 'graphql'
 import * as chai from 'chai'
 import * as sinon from 'sinon'
+import mongoose from '../../../src/mongoose/mongoose'
 import People from '../../../src/graphql/schemas/people'
 
 const expect = chai.expect
@@ -24,5 +25,33 @@ describe('People type fields', () => {
     it('Should have an age field of type Int', () => {
         expect(typeFields).to.have.property('age')
         expect(typeFields.age.type).to.deep.equals(graphql.GraphQLInt)
+    })
+})
+
+describe('People type resolvers', () => {
+    //connect to MongoDB before running GraphQL resolvers tests
+    beforeEach((done) => {
+        mongoose.connect()
+        done()
+    })
+
+    let queryFields = query.getFields()
+
+    it('Should return all people', async () => {
+        sinon.stub(People, "getProjections")
+            .withArgs(undefined)
+            .returns({ firstName: true, lastName: true, age: true })
+
+        let people = await queryFields.people.resolve({}, {})
+
+        expect(people).to.not.be.null
+        expect(people).to.be.a('array')
+        expect(people).to.not.be.empty
+    })
+
+    //disconnect from MongoDB after all GraphQL resolvers tests have finished running
+    afterEach((done) => {
+        mongoose.disconnect()
+        done()
     })
 })
